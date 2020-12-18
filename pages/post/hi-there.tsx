90738d4b6bb3aab5e82a8dc60a7a7e6520a416b9
pl <7350206@pl.com>
2020-12-18 09:32:21 +0300

    seo: add regular <head> to post

--- pages/post/hi-there.tsx
+++ pages/post/hi-there.tsx
@@ -1,10 +1,15 @@
 import Navigation from '@components/Navigation';
+import Head from 'next/head';
 import Image from 'next/image';
 export default function Hi(){
   return (
     <>
       <Navigation/>
-      <div>
+      <Head>
+        <title>Hi there :: my blog</title>
+        <meta property="og:title" content="Hi there :: my blog"/>
+      </Head>
+      <article>
         <h3>Hi there</h3>
         <p>this is a 1st post</p>
         <Image
@@ -13,7 +18,7 @@ export default function Hi(){
           height={640}
         />
         <p>photo by <a href="https://unsplash.com/@anniespratt">Annie Spratt</a> on Unsplash</p>
-      </div>
+      </article>
     </>
   )
 }
