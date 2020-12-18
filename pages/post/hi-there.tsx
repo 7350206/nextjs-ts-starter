7d7f6d032c856ee082021637c1095b71477fd7b8
pl <7350206@pl.com>
2020-12-18 09:18:27 +0300

    image: add optimized image to 1st post

--- pages/post/hi-there.tsx
+++ pages/post/hi-there.tsx
@@ -1,4 +1,5 @@
 import Navigation from '@components/Navigation';
+import Image from 'next/image';
 export default function Hi(){
   return (
     <>
@@ -6,6 +7,12 @@ export default function Hi(){
       <div>
         <h3>Hi there</h3>
         <p>this is a 1st post</p>
+        <Image
+          src="/post/hi-there/road.jpg"
+          width={480}
+          height={640}
+        />
+        <p>photo by <a href="https://unsplash.com/@anniespratt">Annie Spratt</a> on Unsplash</p>
       </div>
     </>
   )
